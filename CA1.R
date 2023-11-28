c359690837e97bca3438c7ea24216a30470ba4c4
Gabriel <gabriel.pa16@gmail.com>
2023-11-28 21:06:54 +0000

    task A done
    I've made the visualizations for all variables data types

--- CA1.R
+++ CA1.R
@@ -1,69 +1,83 @@
-# Load the CSV file into a dataframe
-file_path <- 'full_grouped.csv'
-df <- read.csv(file_path)
-
-# Display
-head(df)
-
-
 library(ggplot2)  
 library(dplyr)    
 library(tidyr)   
 library(ggplot2) 
 
-#Categorical Variables:
- # "Country/Region"
- # "WHO Region"
+# a)
 
-#Discrete Variables:
-  "Confirmed"
-  "Deaths"
-  "Recovered"
-  "Active"
-  "New cases"
-  "New deaths"
-  "New recovered"
+## Data types
 
-#Continuous Variables:
-  "Date"
+### Categorical Variables:
+ #### "Country/Region", "WHO Region"
 
-# importing data frame
+### Discrete Variables:
+  #### "Confirmed", "Deaths" ,"Recovered", "Active", "New cases", "New deaths", "New recovered"
+
+### Continuous Variables:
+  #### "Date"
+
+### Importing data frame
 covid_data <- read.csv('full_grouped.csv', header = TRUE)
 
-# Data Transformation
-#--- luiza transform data here
 
+### Checking missing values
+
+#### Check for missing values
+missing_values <- colSums(is.na(covid_data))
+
+#### Print missing values
+print(missing_values)
+
+#### Plot missing values
+barplot(missing_values, col = "red", main = "Missing Values")
+
+
+#### Checking data frame structure
 str(covid_data)
 
 
 
+### visualization of categorical
 
-
-
-
-
-#visualisation of categorical
-# Assuming your data frame is named 'covid_data'
-# Extract the counts for each country
+#### Extract the counts for each country
 country_counts <- table(covid_data$Country.Region)
 
 
-# Sort countries by count in descending order
+#### Sort countries by count in descending order
 sorted_countries <- names(country_counts[order(-country_counts)])
-# Creating bar plot for the first 10 countries, for demonstration of data type
+
+#### Creating bar plot for the first 10 countries, for demonstration of data type
 barplot(country_counts[sorted_countries[1:10]], col = "skyblue",
         main = "First 10 countries", las= 2, cex.names = 0.7)
 
-# creating bar plot for visualization of WHO Region variable which is categorical
+
+#### creating bar plot for visualization of WHO Region variable which is categorical
 barplot(table(covid_data$WHO.Region),cex.names= 0.7, las=2, col = "lightgreen", main = "WHO Region Distribution")
 
 
 
-# Convert the 'Date' column to a Date object
+### Discrete Variables
+par(mfrow = c(3,3))  # Arrange plots in a 3x3 grid
+
+#### Loop through each numeric variable
+for (variable in c("Confirmed", "Deaths", "Recovered", "Active", "New.cases", "New.deaths", "New.recovered")) {
+  hist(covid_data[[variable]], col = "orange", main = paste(variable, "Distribution"), xlab = variable, ylab = "Frequency")
+}
+
+### Visualization for continuous data
+
+#### Convert the 'Date' column to a Date object
 covid_data$Date <- as.Date(covid_data$Date)
 
-# Plotting time series of Confirmed cases
+#### Create a line plot for the number of cases over time
 plot(covid_data$Date, covid_data$Confirmed, type = "l", col = "blue",
-     xlab = "Date", ylab = "Confirmed Cases",
-     main = "COVID-19 Confirmed Cases Over Time")
+     xlab = "Date", ylab = "Confirmed Cases", main = "COVID-19 Confirmed Cases Over Time")
+
+
+# b)
+
+
+
+
+
 
